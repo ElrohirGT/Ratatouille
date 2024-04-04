5e64cde9fe28141abae26cccf38bd4ea2a7f0d8a
ElrohirGT <elrohirgt@gmail.com>
2024-04-04 00:24:44 -0600

    fix: Added specificity to rm command

--- flake.nix
+++ flake.nix
@@ -67,7 +67,7 @@
           echo -e "$(ansi yellow)"WARNING:"$(ansi reset)" This script must be run on the project root directory!
 
           echo "Trying to remove old .devenv..."
-          rm .devenv || rm -r .devenv || true
+          rm ./.devenv/state/postgres/ || rm -r ./.devenv/state/postgres/ || true
 
           echo "Entering devshell..."
           nix develop --impure . -c devenv up
