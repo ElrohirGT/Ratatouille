408daa958a279b33622389fcd7f5a6f3829eae06
DanielRasho <danirayoroldan@gmail.com>
2024-04-11 10:29:20 -0600

    Fix: Change Menu View method. Without using List bubbles

--- client/internal/tui/components/menu.go
+++ client/internal/tui/components/menu.go
@@ -4,12 +4,9 @@ import (
 	"strings"
 
 	"github.com/ElrohirGT/Ratatouille/internal/tui/styles"
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
-
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type MenuItem struct {
 	Route           string
@@ -22,26 +19,15 @@ func (i MenuItem) Description() string { return i.ItemDescription }
 func (i MenuItem) FilterValue() string { return i.ItemTitle }
 
 type MenuModel struct {
-	Items     []MenuItem
-	list      list.Model
-	FocusItem MenuItem
+	Title        string
+	Items        []MenuItem
+	FocusIndex   int
+	SelectedItem MenuItem
 }
 
 func CreateMenu(title string, items []MenuItem) MenuModel {
 
-	menuItems := make([]list.Item, 0)
-
-	for i := 0; i < len(items); i++ {
-		menuItems = append(menuItems, items[i])
-	}
-
-	// Config List component
-	delegate := list.NewDefaultDelegate()
-	delegate.Styles = styles.GetMenuItemStyle()
-	modelList := list.New(menuItems, delegate, 0, 0)
-	modelList.Title = title
-
-	return MenuModel{Items: items, list: modelList}
+	return MenuModel{Title: title, Items: items}
 }
 
 func (m MenuModel) Init() tea.Cmd {
@@ -51,25 +37,52 @@ func (m MenuModel) Init() tea.Cmd {
 func (m MenuModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		if msg.Type == tea.KeyEnter {
-			i, ok := m.list.SelectedItem().(MenuItem)
-			if ok {
-				m.FocusItem = i
+		switch msg.Type {
+		case tea.KeyTab, tea.KeyShiftTab, tea.KeyEnter, tea.KeyUp, tea.KeyDown:
+			s := msg.Type
+
+			if s == tea.KeyEnter && m.FocusIndex != len(m.Items)-1 {
+				return m, nil
+			}
+			if s == tea.KeyUp || s == tea.KeyShiftTab {
+				m.FocusIndex--
+			} else {
+				m.FocusIndex++
+			}
+
+			if m.FocusIndex > len(m.Items)-1 {
+				m.FocusIndex = 0
+			} else if m.FocusIndex < 0 {
+				m.FocusIndex = len(m.Items) - 1
 			}
 		}
-	case tea.WindowSizeMsg:
-		x, y := docStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-x, msg.Height-y)
 	}
 
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
+	m.SelectedItem = m.Items[m.FocusIndex]
+
+	return m, nil
 }
 
 func (m MenuModel) View() string {
 	var sb strings.Builder
 
-	sb.WriteString(docStyle.Render(m.list.View()))
+	sb.WriteString("\n")
+	sb.WriteString(styles.GetTitleStyle().Render(m.Title))
+	sb.WriteString("\n\n")
+
+	for i, v := range m.Items {
+
+		var titleStyle, descriptionStyle lipgloss.Style
+
+		if i == m.FocusIndex {
+			titleStyle, descriptionStyle = styles.GetMenuItemFocusStyle()
+		} else {
+			titleStyle, descriptionStyle = styles.GetMenuItemDefaultStyle()
+		}
+		sb.WriteString(titleStyle.Render(v.ItemTitle) + "\n" + descriptionStyle.Render(v.Description()) + "\n\n")
+
+	}
+	sb.WriteString(styles.GetHelpStyle().Render("↑ up • ↓ down • Enter select  • Esc quit"))
+
 	return sb.String()
 }
