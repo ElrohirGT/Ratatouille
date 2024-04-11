82de222a747cb10be15f04848d19bae6f254f5ca
DanielRasho <danirayoroldan@gmail.com>
2024-04-10 23:36:15 -0600

    Fix: Title bugging on display, for form component

--- client/internal/tui/components/forms.go
+++ client/internal/tui/components/forms.go
@@ -7,7 +7,6 @@ import (
 	"github.com/ElrohirGT/Ratatouille/internal/tui/styles"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"golang.org/x/exp/maps"
 )
 
 var focusedButton = styles.GetFocusedStyle().Render("[ Submit ]")
@@ -23,26 +22,30 @@ type FormsModel struct {
 	Title      string
 	FormInputs map[string]FormsInput
 
-	FocusIndex int
-	inputs     []textinput.Model
+	FocusIndex  int
+	inputsTitle []string
+	inputs      []textinput.Model
 }
 
 func CreateForms(title string, formsInputs map[string]FormsInput) FormsModel {
 
 	m := FormsModel{
-		Title:      title,
-		FormInputs: formsInputs,
-		inputs:     make([]textinput.Model, 0),
+		Title:       title,
+		FormInputs:  formsInputs,
+		inputsTitle: make([]string, 0),
+		inputs:      make([]textinput.Model, 0),
 	}
 
 	var t textinput.Model
-	for _, v := range formsInputs {
+	for k, v := range formsInputs {
 		t = textinput.New()
 		t.Cursor.Style = styles.GetFocusedStyle()
 		t.PromptStyle = styles.GetDeactivateStyle()
 		t.TextStyle = styles.GetDeactivateStyle()
 		t.CharLimit = 30
 		t.Placeholder = v.Placeholder
+
+		m.inputsTitle = append(m.inputsTitle, k)
 
 		if v.InputType == "password" {
 			t.EchoMode = textinput.EchoPassword
@@ -107,16 +110,15 @@ func (m FormsModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m *FormsModel) updateInputs(msg tea.Msg) tea.Cmd {
 	cmds := make([]tea.Cmd, len(m.inputs))
-	keys := maps.Keys(m.FormInputs)
 
 	// Only text inputs with Focus() set will respond, so it's safe to simply
 	// update all of them here without any further logic.
 	for i := range m.inputs {
 		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
 		// Assigning updated value to each input Entry.
-		entry := m.FormInputs[keys[i]]
+		entry := m.FormInputs[m.inputsTitle[i]]
 		entry.Value = m.inputs[i].Value()
-		m.FormInputs[keys[i]] = entry
+		m.FormInputs[m.inputsTitle[i]] = entry
 	}
 
 	return tea.Batch(cmds...)
@@ -124,14 +126,13 @@ func (m *FormsModel) updateInputs(msg tea.Msg) tea.Cmd {
 
 func (m FormsModel) View() string {
 	var b strings.Builder
-	keys := maps.Keys(m.FormInputs)
 
 	b.WriteString(styles.GetTitleStyle().Render(m.Title))
 	b.WriteRune('\n')
 	b.WriteRune('\n')
 
-	for i := range keys {
-		b.WriteString(fmt.Sprintf("╭─・ %s \n", keys[i]))
+	for i := range m.inputsTitle {
+		b.WriteString(fmt.Sprintf("╭─・ %s \n", m.inputsTitle[i]))
 		b.WriteString(m.inputs[i].View())
 		if i < len(m.inputs)-1 {
 			b.WriteRune('\n')
