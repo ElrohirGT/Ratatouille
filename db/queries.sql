be5e0946773bfdf7b13e1e4157facd0a949e490a
ElrohirGT <elrohirgt@gmail.com>
2024-04-12 22:58:36 -0600

    fix: query

--- db/queries.sql
+++ db/queries.sql
@@ -100,7 +100,7 @@ FROM pedido p
 	INNER JOIN itemMenuCategoria imc ON im.categoria = imc.id
 WHERE 
 	imc.nombre = 'Bebidas' 
-	AND ( e.nombre = 'Pedido' e.nombre = "En preparación" )
+	AND ( e.nombre = 'Pedido' AND e.nombre = "En preparación" )
 ORDER BY p.fecha DESC;
 
 -- name: SetOrderPreparing :exec
